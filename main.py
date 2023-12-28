ee22fb81c130b1a23a188aff0bca1b22b0d23118
Miss <missakanksha1122@gmail.com>
2023-12-28 15:57:50 +0530

    web site

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/qwe") #decorator
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/firstpage")
+def first_page():
+    return render_template('100xcohort.html')
